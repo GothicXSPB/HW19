7d22ab59010edf378a747865ac4a3a5105cb14a7
GothicXSPB <ZZZerggg@mail.ru>
2022-09-11 18:38:48 +0300

    Add project files.

---
+++ Massive2/Math.cs
@@ -0,0 +1,149 @@
+﻿
+using System;
+
+namespace Massive2
+{
+    public class Math
+    {
+        public static int[,] Create()
+        {
+            int[,] array = new int[5, 5];
+            Random r = new Random();
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = r.Next(100, 1000);
+                }
+            }
+            return array;
+        }
+
+        public static void Show(int[,] array)
+
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($"{array[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void ShowConsole(int result, string line)
+        {
+            Console.WriteLine($"{line}: {result}");
+        }
+        public static void ShowConsole(int[] result, string line)
+        {
+            Console.Write(line);
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.Write(result[i] + "; ");
+            }
+        }
+
+        //1) Найти минимальный элемент массива
+
+        public static int GetMin(int[,] array)
+        {
+            int min = array[0, 0];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] < min)
+                    {
+                        min = array[i, j];
+                    }
+                }
+            }
+            return min;
+        }
+
+        //2) Найти максимальный элемент массива
+
+        public static int GetMax(int[,] array)
+        {
+            int max = array[0, 0];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] > max)
+                    {
+                        max = array[i, j];
+                    }
+                }
+            }
+            return max;
+        }
+
+        //3) Найти индекс минимального элемента массива
+
+        public static int[] GetMinIndex(int[,] array)
+        {
+            int indexI = 0;
+            int indexJ = 0;
+            int min = array[0, 0];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] < min)
+                    {
+                        min = array[i, j];
+                        indexI = i;
+                        indexJ = j;
+                    }
+                }
+            }
+            return new[] { indexI, indexJ };
+        }
+
+        //4) Найти индекс максимального элемента массива
+
+        public static int[] GetMaxIndex(int[,] array)
+        {
+            int indexI = 0;
+            int indexJ = 0;
+            int max = array[0, 0];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] > max)
+                    {
+                        max = array[i, j];
+                        indexI = i;
+                        indexJ = j;
+                    }
+                }
+            }
+            return new[] { indexI, indexJ };
+        }
+
+        //5) Найти количество элементов массива, которые больше своих левого и нижнего соседа одновременно.
+
+        public static int AmountMinElement(int[,] array)
+        {
+            int result = 0;
+
+            for (int i = 1; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1) - 1; j++)
+                {
+                    if (array[i, j] > array[i - 1, j] && array[i, j] > array[i, j + 1])
+                    {
+                        result++;
+                    }
+                }
+
+            }
+            return result;
+        }
+    }
+}
---
+++ Massive2/Program.cs
@@ -0,0 +1,19 @@
+﻿using Massive2;
+
+public class Program
+{
+    public static void Main(string[] arg)
+    {
+        int[,] array = Math.Create();
+        Math.Show(array);
+        int min = Math.GetMin(array);
+        Math.ShowConsole(min, "min");
+        int[] indexMin = Math.GetMinIndex(array);
+        Math.ShowConsole(indexMin, "IndexMin: ");
+        int[] indexMax = Math.GetMaxIndex(array);
+        Math.ShowConsole(indexMax, "IndexMax: ");
+        int amount = Math.AmountMinElement(array);
+        Math.ShowConsole(amount, "Number of minimum values: ");
+
+    }
+}
